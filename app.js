bb9924288e50d19e21f381c3592a11e1e2ab3df9
Sean Rooney <seandanielrooney@gmail.com>
2022-01-22 20:10:16 -0700

    cleaning up

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 // THIS SCROLLS THE CENTER DIV INTO CENTER VIEW AND PREPARES THE APP
 
-function myFunction() {
-  alert("Page is loaded");
+function centeringFunction() {
   var elmnt = document.getElementById("moss");
   elmnt.scrollIntoView();
 }
 
-
-// THIS IS A FUNCTION THAT DETECTS SCROLL BEHAVIOR 
+// THIS IS A FUNCTION THAT DETECTS SCROLL BEHAVIOR
 
 let lastKnownScrollPosition = 0;
 let ticking = false;
 
-function doSomething(scrollPos) {
-
-// THIS IS THE BEHAVIOR THAT WILL BE EXECUTED UPON DETECTION OF SCROLLING
-const newDiv = document.createElement("div");
+function generatingFunction(scrollPos) {
+  // THIS IS THE BEHAVIOR THAT WILL BE EXECUTED UPON DETECTION OF SCROLLING
+  const newDiv = document.createElement("div");
 
   // and give it some content
-  const newContent = document.createTextNode("Hi there and greetings!");
+  const newContent = document.createTextNode("I'm Grombo!");
 
   // add the text node to the newly created div
   newDiv.appendChild(newContent);
@@ -26,15 +23,14 @@ const newDiv = document.createElement("div");
   // add the newly created element and its content into the DOM
   const currentDiv = document.getElementById("div1");
   document.body.insertBefore(newDiv, currentDiv);
+}
 
-};
-
-document.addEventListener('scroll', function(e) {
+document.addEventListener("scroll", function (e) {
   lastKnownScrollPosition = window.scrollY;
 
   if (!ticking) {
-    window.requestAnimationFrame(function() {
-      doSomething(lastKnownScrollPosition);
+    window.requestAnimationFrame(function () {
+      generatingFunction(lastKnownScrollPosition);
       ticking = false;
     });
 
