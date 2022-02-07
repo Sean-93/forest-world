c11474b5c4edc300deb3f7ce781363d72f5231ce
unknown <seandanielrooney@gmail.com>
2022-02-06 16:38:48 -0800

    found possible solution to hard coding a large array of images

--- app.js
+++ app.js
@@ -7,6 +7,27 @@ function centeringFunction() {
 
 // THIS IS AN ARRAY WHERE THE IMAGES ARE STORED
 
+//AT THIS POINT THE CODE BELOW (REFERENCING XHR) WAS JUST COPIED AND PASTED AND NEEDS TO BE REFACTORED TO BE COMPATIBLE W/ EXISTING CODE
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "/img", true);
+xhr.responseType = 'document';
+xhr.onload = () => {
+  if (xhr.status === 200) {
+    var elements = xhr.response.getElementsByTagName("a");
+    for (x of elements) {
+      if ( x.href.match(/\.(jpe?g|png|gif)$/) ) { 
+          let img = document.createElement("img");
+          img.src = x.href;
+          document.body.appendChild(img);
+      } 
+    };
+  } 
+  else {
+    alert('Request failed. Returned status of ' + xhr.status);
+  }
+}
+xhr.send()
 
 
 // THIS IS A FUNCTION THAT DETECTS SCROLL BEHAVIOR 
